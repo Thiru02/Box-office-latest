bf2d12c85f1fe9e273cf3605e4311ffb5b863679
Thiru <thiruparanb@gmail.com>
2022-10-03 13:16:12 +0530

    API added to search

--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Mainpage from '../Mainpage';
 
 const Homepage = () => {
-  return <Mainpage>This is home Page</Mainpage>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = key => {
+    if (key.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <Mainpage>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button onClick={() => onSearch()}>Search</button>
+    </Mainpage>
+  );
 };
 
 export default Homepage;
