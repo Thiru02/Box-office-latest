b1c9b142b8540983060cc3d1d3ee0b7ed1a64749
Thiru <thiruparanb@gmail.com>
2022-10-04 15:12:12 +0530

    Radio buttons added for acotrs and shows

--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,22 +1,51 @@
 import React, { useState } from 'react';
 import Mainpage from '../Mainpage';
+import { apiGet } from '../../misc/config';
 
 const Homepage = () => {
   const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [currentSearch, setCurrentSearch] = useState('shows');
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
+  const isSearchShows = currentSearch === 'shows';
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    apiGet(`/search/${currentSearch}?q=${input}`).then(result => {
+      setOutput(result);
+    });
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then(r => r.json())
+    //   .then(result => {
+    //     console.log(result);
+    //     setOutput(result);
+    //   });
   };
   const onKeyDown = key => {
     if (key.keyCode === 13) {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    if (output?.length === 0) {
+      return <div>no results</div>;
+    }
+    if (output?.length > 0) {
+      return (
+        <div>
+          {output[0].show
+            ? output.map(val => <div key={val.show.id}>{val.show.name}</div>)
+            : output.map(val => (
+                <div key={val.person.id}>{val.person.name}</div>
+              ))}
+        </div>
+      );
+    }
+    return null;
+  };
+  const onSearchChange = ev => {
+    setCurrentSearch(`${ev.target.value}`);
   };
   return (
     <Mainpage>
@@ -25,8 +54,30 @@ const Homepage = () => {
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
+        placeholder="Search for something"
       />
       <button onClick={() => onSearch()}>Search</button>
+      <div>
+        <label>
+          Shows
+          <input
+            checked={isSearchShows}
+            type="radio"
+            onChange={onSearchChange}
+            value="shows"
+          ></input>
+        </label>
+        <label>
+          Actors
+          <input
+            checked={!isSearchShows}
+            type="radio"
+            onChange={onSearchChange}
+            value="people"
+          ></input>
+        </label>
+      </div>
+      {renderResults()}
     </Mainpage>
   );
 };
