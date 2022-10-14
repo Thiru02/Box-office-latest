a27b873b93ad822a8706ef60f26fdfe5957beab3
Thiru <thiruparanb@gmail.com>
2022-10-14 09:30:10 +0530

    Show page added and pages styled

--- src/Components/Pages/Show.js
+++ src/Components/Pages/Show.js
@@ -1,26 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { apiGet } from '../../misc/config';
+import Details from '../Show/Details';
+import ShowMainData from '../Show/ShowMainData';
+import Seasons from '../Show/Seasons';
+import Cast from '../Show/Cast';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const Show = () => {
   const { id } = useParams();
-  const [detail, setDetail] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [detail, setDetail] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  const initialState = {
+    isLoading: true,
+    error: null,
+    detail: '',
+  };
+  const reducer = (prevState, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS': {
+        return { isLoding: false, error: null, detail: action.show };
+      }
+      case 'FETCH_FAILED': {
+        return { ...prevState, isLoading: false, error: action.error };
+      }
+      default:
+        return prevState;
+    }
+  };
 
+  const [{ isLoading, error, detail }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   useEffect(() => {
     let isMounted = true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(response => {
         if (isMounted) {
-          setDetail(response);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: response });
+          // setDetail(response);
+          // setIsLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+          // setError(err.message);
+          // setIsLoading(false);
         }
       });
     return () => {
@@ -28,13 +56,36 @@ const Show = () => {
     };
   }, [id]);
   console.log(detail);
-  if (isLoading) {
-    return <div>Data is being loaded</div>;
-  }
-  if (error) {
-    return <div>error occurred:{error}</div>;
-  }
-  return <div>Show</div>;
+  // if (isLoading) {
+  //   return <div>Data is being loaded</div>;
+  // }
+  // if (error) {
+  //   return <div>error occurred:{error}</div>;
+  // }
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        name={detail.name}
+        image={detail.image}
+        rating={detail.rating}
+        tags={detail.tags}
+        summary={detail.summary}
+      />
+      <InfoBlock>
+        <Details
+          Status={detail.status}
+          premiered={detail.premiered}
+          network={detail.network}
+        />
+      </InfoBlock>
+      <InfoBlock>
+        <Seasons seasons={detail?._embedded?.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <Cast cast={detail?._embedded?.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
